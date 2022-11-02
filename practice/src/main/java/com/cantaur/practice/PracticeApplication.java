e5196c6c7205e693f38944897038b38d59f7180c
kaeun <attackmood@gmail.com>
2022-11-02 18:25:13 +0900

    Spring Batch 연습

--- practice/src/main/java/com/cantaur/practice/PracticeApplication.java
+++ practice/src/main/java/com/cantaur/practice/PracticeApplication.java
@@ -1,9 +1,11 @@
 package com.cantaur.practice;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication()
+@EnableBatchProcessing
 public class PracticeApplication {
 
 	public static void main(String[] args) {
---
+++ practice/src/main/java/com/cantaur/practice/config/batch/ExampleJobConfig.java
@@ -0,0 +1,103 @@
+package com.cantaur.practice.config.batch;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+@RequiredArgsConstructor
+public class ExampleJobConfig {
+
+    private final JobBuilderFactory jobBuilderFactory;
+    private final StepBuilderFactory stepBuilderFactory;
+
+
+    @Bean
+    public Job ExampleJob(){
+
+        Job exampleJob = jobBuilderFactory.get("exampleJob")
+                .start(startStep()).build();
+
+        return exampleJob;
+    }
+
+
+    @Bean
+    @JobScope
+    public Step exampleStep() throws Exception {
+        return stepBuilderFactory.get("startStep")
+                .chunk(10)
+                .build();
+    }
+
+
+
+
+
+    @Bean
+    public Step startStep() {
+        return stepBuilderFactory.get("startStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Start Step!");
+
+                    String result = "COMPLETED";
+                    //String result = "FAIL";
+                    //String result = "UNKNOWN";
+
+                    //Flow에서 on은 RepeatStatus가 아닌 ExitStatus를 바라본다.
+                    if(result.equals("COMPLETED"))
+                        contribution.setExitStatus(ExitStatus.COMPLETED);
+                    else if(result.equals("FAIL"))
+                        contribution.setExitStatus(ExitStatus.FAILED);
+                    else if(result.equals("UNKNOWN"))
+                        contribution.setExitStatus(ExitStatus.UNKNOWN);
+
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step failOverStep(){
+        return stepBuilderFactory.get("nextStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("FailOver Step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+    @Bean
+    public Step processStep(){
+        return stepBuilderFactory.get("processStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Process Step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+
+    @Bean
+    public Step writeStep(){
+        return stepBuilderFactory.get("writeStep")
+                .tasklet((contribution, chunkContext) -> {
+                    log.info("Write Step!");
+                    return RepeatStatus.FINISHED;
+                })
+                .build();
+    }
+
+}
